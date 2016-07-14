2adff7ce7789163d411fb57be530822935484bb3
Chris <chris@ntlp.org.uk>
2016-07-14 09:02:14 +0100

    Create Hack_3_User_Messages.py
    This hack utilises the Python shell to interact with the user. The use of the "While" loop ensures that our program runs forever. The shell will display the message "What should we say?". prompting the user to type a message. Once the user has typed the message and pressed enter, the input is saved as a string variable and displayed in the chat pane. Once the users message has been displayed in Minecraft the Python shell then resets and asks the question again.

---
+++ Hack_3_User_Messages.py
@@ -0,0 +1,9 @@
+import minecraft.minecraft as minecraft
+import time
+mc = minecraft.Minecraft.create()
+
+while True:
+    time.sleep(5)
+    text = raw_input("What should we say?")
+    time.sleep (5)
+    mc.postToChat (text)

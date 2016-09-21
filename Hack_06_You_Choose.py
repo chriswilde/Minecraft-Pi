4a144007855e17e96ced0eb2b3fa84bd7681a3e8
Chris <chris@ntlp.org.uk>
2016-09-21 11:02:06 +0100

    Create Hack_06_You_Choose.py

---
+++ Hack_06_You_Choose.py
@@ -0,0 +1,12 @@
+import minecraft.minecraft as minecraft
+import time
+mc = minecraft.Minecraft.create()
+
+while True:
+    xPos = raw_input("Where would you like to go on the x axis?")
+    yPos = raw_input("Where would you like to go on the y axis?")
+    zPos = raw_input("Where would you like to go on the z axis?")
+
+    time.sleep(5)
+    mc.player.setPos (xPos, yPos, zPos)
+    mc.postToChat("You are now at: x = "+xPos+", y = "+yPos+", z = "+yPos)

ce98f70d043c695c937a7a837f33b4b0dc95b5a2
Chris <chris@ntlp.org.uk>
2017-01-18 09:58:56 +0000

    Add files via upload

---
+++ Hack_13_Square_This_Way.py
@@ -0,0 +1,17 @@
+import minecraft.minecraft as minecraft
+import time
+mc = minecraft.Minecraft.create()
+
+mc.player.setPos(85,1,-20)
+
+x = 95
+y = 2
+z = -23
+
+for i in range (6):
+    for i in range (6):
+        mc.setBlock(x,y,z,103)
+        y=y+1
+        time.sleep(0.2)
+    z=z+1
+    y=y-6

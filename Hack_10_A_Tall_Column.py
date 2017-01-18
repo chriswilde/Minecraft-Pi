c27cd7c3555670f141f78414c745c27c009a07d2
Chris <chris@ntlp.org.uk>
2017-01-18 09:57:57 +0000

    Add files via upload

---
+++ Hack_10_A_Tall_Column.py
@@ -0,0 +1,15 @@
+import minecraft.minecraft as minecraft
+import time
+mc = minecraft.Minecraft.create()
+
+xPos = 92
+yPos = 1
+zPos = -102
+
+while yPos < 50:
+    blockType = 103
+    mc.setBlock(xPos, yPos, zPos, blockType)
+    yPos = yPos + 1
+    time.sleep (0.2)
+
+    

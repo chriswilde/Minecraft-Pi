9b13c0cddfe712e07fb67954e41513bbbd961140
Chris <chris@ntlp.org.uk>
2017-01-18 09:59:51 +0000

    Add files via upload

---
+++ Hack_17_Let_It_Go.py
@@ -0,0 +1,11 @@
+import minecraft.minecraft as minecraft
+import time
+import minecraft.block as block
+mc = minecraft.Minecraft.create()
+
+while True:
+    xPos = mc.player.getPos().x
+    yPos = mc.player.getPos().y - 1
+    zPos = mc.player.getPos().z
+    if mc.getBlockWithData(xPos,yPos,zPos) == block.GLOWING_OBSIDIAN:
+        mc.setBlock(xPos,yPos,zPos,79)

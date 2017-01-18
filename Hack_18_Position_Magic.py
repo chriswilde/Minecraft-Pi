dfd8a09e8950204fac54a4324d1dd1adeddb1913
Chris <chris@ntlp.org.uk>
2017-01-18 10:00:08 +0000

    Add files via upload

---
+++ Hack_18_Position_Magic.py
@@ -0,0 +1,21 @@
+import minecraft.minecraft as minecraft
+import time
+mc = minecraft.Minecraft.create()
+
+while True:
+    pos = mc.player.getTilePos()
+
+    if 100 <= pos.x <= 115 and 50 <= pos.z <= 60:
+        mc.setBlocks(101,1,51,109,2,59,48)
+
+    elif 100 <= pos.x <= 110 and 60 <= pos.z <= 65:
+        mc.setBlocks(101,1,51,109,2,59,57)
+
+    elif 95 <= pos.x <= 100 and 50 <= pos.z <= 60:
+        mc.setBlocks(101,1,51,109,2,59,103)
+
+    elif 100 <= pos.x <= 110 and 45 <= pos.z <= 50:
+        mc.setBlocks(101,1,51,109,2,59,74)
+
+    else:
+        mc.setBlocks(101,1,51,109,2,59,247)

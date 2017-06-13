5e901379b3507fac692cc9e2c6aecbc3e349edfd
Chris <chris.wilde1978@gmail.com>
2017-06-13 20:13:54 +0100

    Create Hack_01_Hello_World.py

--- Hack_01_Hello_World.py
+++ Hack_01_Hello_World.py
@@ -1,6 +1,6 @@
-import minecraft.minecraft as minecraft
+from mcpi.minecraft import Minecraft
 import time
-mc = minecraft.Minecraft.create()
+mc = Minecraft.create()
 
 time.sleep(5)
 mc.postToChat("Hello World")
